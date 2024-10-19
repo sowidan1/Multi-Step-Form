7dcb5afbf704c579feb77a052b3df7dc485507a8
sowidan1 <osamasowidan1@gmail.com>
2024-10-19 14:47:09 +0300

    add multi form

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>How to Create Multi Step Form in Laravel 11</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+</head>
+<body style="background-color: #f3fdf3">
+
+<div class="container">
+    <h2> Laravel 11 Multi Step Form Example</h2>
+    @yield('content')
+</div>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(\App\Http\Controllers\ProductController::class)->group(function () {
+    Route::get('products','index')->name('products.index');
+
+    Route::get('products/create-step-one','createStepOne')->name('products.create.step.one');
+    Route::post('products/create-step-one','postCreateStepOne')->name('products.create.step.one.post');
+    Route::get('products/create-step-two','createStepTwo')->name('products.create.step.two');
+    Route::post('products/create-step-two','postCreateStepTwo')->name('products.create.step.two.post');
+    Route::get('products/create-step-three','createStepThree')->name('products.create.step.three');
+    Route::post('products/create-step-three','postCreateStepThree')->name('products.create.step.three.post');
 });
